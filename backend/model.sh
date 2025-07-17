abd9aa571455e0ab2cbc4025be5c10ea27bf7bb8
Raghul-M <raghul.m1430@gmail.com>
2025-07-17 06:49:07 +0530

    fixed ollama

--- backend/model.sh
+++ backend/model.sh
@@ -5,8 +5,7 @@ if ! command -v ollama &> /dev/null; then
     curl -fsSL https://ollama.com/install.sh | sh
 fi
 
-# Start Ollama in background
-ollama serve &
+
 
 # Pull models
 ollama pull llama3.2:1b
@@ -15,4 +14,6 @@ ollama pull deepseek-llm:67b
 ollama pull gemma3:27b
 ollama pull nomic-embed-text:v1.5
 
+# Start Ollama in background
+ollama serve &
 
